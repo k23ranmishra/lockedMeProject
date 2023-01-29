6456a3aeefe7862a53cbaba7e8b913ac7d95c6ca
k23ranmishra <122270707+k23ranmishra@users.noreply.github.com>
2023-01-30 00:44:11 +0530

    Create menuList.java
    This class shows menu options

---
+++ menuList.java
@@ -0,0 +1,26 @@
+
+
+public class menuList {
+	
+	void displayMenu(){
+		
+	System.out.println("\n.....Menu.............");
+	
+	System.out.println(" 2. List all files\n");
+	
+	System.out.println(" 3. Add a file\n");
+	
+	System.out.println(" 4. Delete a file\n");
+	
+	System.out.println(" 5. Search a file\n");
+	
+	System.out.println(" 6. exit\n");
+	
+	
+		
+	}
+	
+	
+
+}
+
